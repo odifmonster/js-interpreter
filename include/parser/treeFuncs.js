8d3bf88bbf8f3eccd265c2560a49672bec43b09c
odifmonster <lamanwyner@gmail.com>
2024-12-14 16:55:49 -0500

    grammar accepts programs with variable initialization, if statements, and while loops

--- include/parser/treeFuncs.js
+++ include/parser/treeFuncs.js
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 const empty = { kind: "empty" };
 
 export function mkNumber(x) {
@@ -34,6 +35,10 @@ export function mkAugUnop(op) {
   return { kind: "augunop", op: op, child: empty };
 }
 
+export function mkBlock(res) {
+  return { kind: "block", body: res[1] };
+}
+
 export function buildBinopTree(res) {
   const first = res.shift();
   return res.reduce((acc, e, i) => {
@@ -57,4 +62,30 @@ export function buildPostUnopTree(res) {
     e.child = acc;
     return e;
   }, first);
+}
+
+export function buildStmt(res) {
+  return { kind: "exp", exp: res[0] };
+}
+
+export function buildInitVarStmt(res) {
+  return {
+    kind: res[0],
+    name: res[1],
+    value: res[3]
+  };
+}
+
+export function buildIfStmt(res) {
+  const stmt = {
+    kind: "if", test: res[2], truePart: res[4], falsePart: empty
+  };
+  if (res.length === 7) stmt.falsePart = res[6];
+  return stmt;
+}
+
+export function buildWhileStmt(res) {
+  return {
+    kind: "while", test: res[2], body: res[4]
+  };
 }
